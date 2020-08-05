1abdd8eb8f08a92cf567711668dbdf44c25d915e
hirocaster <hohtsuka@gmail.com>
2020-08-05 16:24:13 +0900

    Disable output error exception message in test

--- test/ratchet_wrench_test.exs
+++ test/ratchet_wrench_test.exs
@@ -264,8 +264,9 @@ defmodule RatchetWrenchTest do
 
     assert RatchetWrench.TransactionManager.exist_transaction? == false
 
-    {:error, error} =
-      RatchetWrench.transaction fn ->
+    assert capture_log(fn ->
+      {:error, error} =
+        RatchetWrench.transaction fn ->
         assert RatchetWrench.TransactionManager.exist_transaction?
 
         RatchetWrench.transaction fn ->
@@ -279,10 +280,11 @@ defmodule RatchetWrenchTest do
         raise "error from transaction"
 
         {:ok, _singer} = RatchetWrench.Repo.insert(%Singer{singer_id: singer_id2,
-                                                             first_name: "trans func #{singer_id2}"})
+                                                           first_name: "trans func #{singer_id2}"})
       end
 
-    assert %RuntimeError{message: "error from transaction"} = error
+      assert %RuntimeError{message: "error from transaction"} = error
+    end) =~ "error from transaction"
 
     assert RatchetWrench.Repo.get(Singer, [singer_id1]) == nil
     assert RatchetWrench.Repo.get(Singer, [singer_id2]) == nil
@@ -296,8 +298,9 @@ defmodule RatchetWrenchTest do
 
     assert RatchetWrench.TransactionManager.exist_transaction? == false
 
-    {:error, error} =
-      RatchetWrench.transaction fn ->
+    assert capture_log(fn ->
+      {:error, error} =
+        RatchetWrench.transaction fn ->
         assert RatchetWrench.TransactionManager.exist_transaction?
 
         RatchetWrench.transaction fn ->
@@ -308,10 +311,11 @@ defmodule RatchetWrenchTest do
           raise "error from transaction"
         end
         {:ok, _singer} = RatchetWrench.Repo.insert(%Singer{singer_id: singer_id2,
-                                                             first_name: "trans func #{singer_id2}"})
+                                                           first_name: "trans func #{singer_id2}"})
       end
 
-    assert %RuntimeError{message: "error from transaction"} = error
+      assert %RuntimeError{message: "error from transaction"} = error
+    end) =~ "error from transaction"
 
     assert RatchetWrench.Repo.get(Singer, [singer_id1]) == nil
     assert RatchetWrench.Repo.get(Singer, [singer_id2]) == nil
